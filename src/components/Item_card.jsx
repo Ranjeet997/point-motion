667880d16ded6df6996bfb958a294b6e7788f8a5
Ranjeet <ranjeetkumar9264@gmail.com>
2022-05-12 08:19:02 +0530

    first commit

--- src/components/Item_card.jsx
+++ src/components/Item_card.jsx
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 export default function Item_card() {
   let initialState = useSelector((state) => state);
   console.log(initialState);
-  return initialState.userReducer.map((user)=>{
+  
   return (
-    <div>
-      <div className="addCard mt-5">
-        <div className=" border-bottom">
-          <div
-            className="border-top border-bottom py-3"
-          >
-            <div className="textBox d-flex justify-content-between">
-            <span className="h4">Name</span>
-            <span className="h4">Desctription</span>
-            <span className="h4">Price</span>
-            </div>
-          </div>
-          <div className="row g-0">
-            <div className="col-md-1">
-              <img src="" className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-11">
-              <div className="card-body">
-                <div className="d-flex justify-content-between mx-5 pb-2">
-                  <span className='h6'>{user.prodName}</span>
-                  <span className="h6">{user.prodDes}</span>
-                  <span className='h6'>{user.prodPrice}</span>
-                  <span className="h6 text-info"><a href="">Remove</a></span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    // })
+    <>
+      
+
+    </>
   );
-  })
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,9 @@ export default function Navbar() {
     <div className=''>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-            {/* <a className="navbar-brand" href="#">Navbar</a> */}
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                {/* <li className="nav-item"> */}
                 <Link to='/' className="nav-link active navbar-brand">Home</Link>
-                {/* </li> */}
-                {/* <li className="nav-item"> */}
                 <Link to='item' className="nav-link active navbar-brand">Manage Products</Link>
-                {/* </li> */}
             </ul>
         </div>
         </nav>
